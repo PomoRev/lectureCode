1d3173d72048e09646512945357ab2d2d513c5fa
ProfFrank <emanuef@algonquincollege.com>
2022-07-12 15:58:15 -0400

    midclass update

---
+++ linear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -u
+#
+# Linear Script
+# Prof. Frank Emanuel
+
+file_name='hello'
+file_extension='txt'
+
+touch $file_name'.'$file_extension
+
+ls -l *'.'$file_extension
+
---
+++ linearwcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -u
+#
+# Linear with Check Script
+# Prof. Frank Emanuel
+
+file_name='hello'
+file_extension='txt'
+
+# touch $file_name'.'$file_extension
+
+
+if [[ -e $file_name'.'$file_extension ]]
+then
+
+    echo "Whoah! File already exists!" >&2
+
+else
+
+    echo "Hey What are you doing?" > $file_name'.'$file_extension
+    pwd
+    ls -l *'.'$file_extension
+
+fi 
+
+
+
+
